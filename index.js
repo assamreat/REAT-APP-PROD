77cf8c2e048a7ab7e55c1d00395be9db09e4665c
Bidyut Deka <bidyutranjan464@gmail.com>
2023-02-13 13:42:20 +0530

    start at 10000 port

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.use('/payment', paymentRoutes);
 // setInterval(queryApi, 900000);
 
 // Define PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Model relations
 Appeal.belongsTo(Appellant, { constraints: true, onDelete: 'CASCADE' });
